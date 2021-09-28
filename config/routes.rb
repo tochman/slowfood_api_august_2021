0efb09651472fe3c5a8fd36f54004a8d2a07fa13
johnnysee <js.johnny.see@gmail.com>
2021-09-28 14:22:08 +0200

    Added a route to products, generated controller, model Unit test is passing but not one of the acceptance test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
+    get 'products/index'
+  end
+  namespace :api do
+    resources :products, only: [:index]
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
     end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe Product, type: :model do
+  describe 'DB table' do
+    it { is_expected.to have_db_column :name }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name }
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(create(:product)).to be_valid
+    end
+  end
+end
+
