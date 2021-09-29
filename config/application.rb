f8a02c276b3f8ba41c1dcff62175c39bf10a3799
maxarvid <max.arvid.anderson@gmail.com>
2021-09-29 09:08:41 +0200

    Corrects typo in specific product spec, removes commented out code in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -27,14 +27,3 @@ module SlowfoodApi
     end
   end
 end
-
-# config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins '*'
-#     resource '*',
-#       headers: :any,
-#       methods: %i[get post put delete],
-#       expose: %w(access-token expiry token-type uid client),
-#       max_age: 0
-#   end
-# end
