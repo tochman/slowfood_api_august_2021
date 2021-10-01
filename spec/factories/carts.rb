6748dc6105f259bb6352696b271ba254d92f65e2
Fraser <Fraser.Hughes@btinternet.com>
2021-10-01 16:33:04 +0200

    Adds unit tests for first product in cart

--- spec/factories/carts.rb
+++ spec/factories/carts.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :cart do
-    
+    name { 'MyString' }
+    unit_price { 500 }
+    quantity { 2 }
+    price { 1000 }
   end
 end
--- spec/requests/api/api_recieves_orders_spec.rb
+++ spec/requests/api/api_recieves_orders_spec.rb
@@ -1,13 +1,31 @@
-RSpec.describe('GET /api/carts') do
+RSpec.describe('GET api/carts') do
   describe 'successfully' do
-    let!(:something) { create(:cart, name: 'Kangaroo Steak', unit_price: 500, quantity: 2, price: 1000) }
+    
+    let!(:carts) { create(:cart, name: 'Kangaroo Steak', unit_price: 500, quantity: 2, price: 1000) }
 
     before do
       get '/api/carts/show'
     end
 
-    it 'is expected to return a 200 response status' do
+    it 'is expected to return a 204 response status' do
       expect(response).to have_http_status 204
+    end
+
+    it 'is expected to return an item name' do
+      # binding.pry
+      expect(Cart.first['name']).to eq "Kangaroo Steak"
+    end
+
+    it 'is expected to return a unit price' do
+      expect(Cart.first['unit_price']).to eq 500
+    end
+
+    it 'is expected to return a quantity' do
+      expect(Cart.first['quantity']).to eq 2
+    end
+
+    it 'is expected to return a overall price' do
+      expect(Cart.first['price']).to eq 1000
     end
   end
 end
