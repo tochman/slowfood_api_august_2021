1b1fe10af2084846b903b0afa259c9df825073a8
Dorian Buck <dorianbuck@gmail.com>
2021-10-06 16:03:20 +0200

    adds implementaion code to controller. Tests are still not passing

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -6,11 +6,16 @@ class Api::CartsController < ApplicationController
     product = Product.find(params['product_id'])
     cart = current_user.carts.create
     cart.cart_products.create(product_id: product.id)
-    render_response(cart, product)
+    render_response(cart, product, 201)
   end
 
   def update
+    new_product = Product.find(params['product_id'])
+    # binding.pry
+    cart = current_user.carts.update(cart)
+    cart.cart_products.create(product_id: new_product.id)
     
+    render_response(cart, product, 200)
   end
 
   private
@@ -19,13 +24,13 @@ class Api::CartsController < ApplicationController
     render json: { message: 'Product not found!' }, status: 422
   end
 
-  def render_response(cart, product)
+  def render_response(cart, product, status)
     render json: {
       message: "#{product.name} was added to your cart!",
       cart: {
         id: cart.id,
         products: cart.products
       }
-    }, status: 201
+    }, status: status
   end
 end
--- spec/requests/api/api_adds_several_products_to_the_cart_spec.rb
+++ spec/requests/api/api_adds_several_products_to_the_cart_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe 'PUT /api/carts', type: :request do
   subject { response }
   let(:product_1) { create(:product, name: 'Kangaroo Steak') }
-  let(:product_2) { create(:product, name: 'Whale Blubber Stew') }
+  let(:product) { create(:product, name: 'Whale Blubber Stew') }
   let(:user) { create(:user) }
   let(:cart) { create(:cart, user_id: user.id) }
   let(:auth_headers) { user.create_new_auth_token }
@@ -10,7 +10,8 @@ RSpec.describe 'PUT /api/carts', type: :request do
       cart.cart_products.create(product_id: product_1.id)
       put "/api/carts/#{cart.id}",
           params: {
-            products: product_2.id
+            product_id: product.id,
+            cart_id: cart.id
           },
           headers: auth_headers
     end
@@ -18,6 +19,7 @@ RSpec.describe 'PUT /api/carts', type: :request do
     it { is_expected.to have_http_status 200 }
 
     it 'is expected to return an array of items' do
+      binding.pry
       expect(response_json['cart']['products'].count).to eq 2
     end
 
